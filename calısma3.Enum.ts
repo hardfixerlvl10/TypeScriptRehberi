826012f1770e7a5fd140783147e0946821c94926
Hamza Demirci <hardfixer675@gmail.com>
2026-05-08 10:47:57 +0300

    calısma3.Enum.ts

---
+++ calısma3.Enum.ts
@@ -0,0 +1,35 @@
+enum HavaDurumu {
+    Gunesli,
+    Yagmurlu,
+    Bulutlu
+}
+
+// 'durum' isminde bir parametre alıyoruz, tipi 'HavaDurumu' enum'ı
+function tavsiyeVer(durum: HavaDurumu) {
+    if (durum === HavaDurumu.Gunesli) {
+        console.log("Gözlüğünü yanına al");
+    } else if (durum === HavaDurumu.Yagmurlu) {
+        console.log("Şemsiyeni yanına al");
+    } else if (durum === HavaDurumu.Bulutlu) {
+        console.log("Montunu yanına al");
+    }
+}
+
+// Fonksiyonu çağırmayı unutma:
+tavsiyeVer(HavaDurumu.Gunesli);
+
+
+//Switch-Case 
+
+function tavsiyeVerPro(durum: HavaDurumu) {
+    switch (durum) {
+        case HavaDurumu.Gunesli:
+            console.log("Gözlük al!");
+            break;
+        case HavaDurumu.Yagmurlu:
+            console.log("Şemsiye al!");
+            break;
+        default:
+            console.log("Hava normal, devam et.");
+    }
+}
